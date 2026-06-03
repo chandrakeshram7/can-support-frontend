40f7e8a0eac79393e6b7e7a6b22ad934ecdb95ae
Chandrakesh <chandrakeshram7@gmail.com>
2026-06-03 09:08:45 +0530

    Updated the backend urls

--- src/lib/auth-context.tsx
+++ src/lib/auth-context.tsx
@@ -62,7 +62,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   try {
 
     const response = await axios.post(
-      "http://192.168.1.105:8080/auth/login",
+      "https://can-support-backend.onrender.com/auth/login",
 
       {
         username,
